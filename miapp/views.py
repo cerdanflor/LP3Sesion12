de70da09c3b1300a6eea089600e8938f8516bb6c
Daniel Levano <levanod@gmail.com>
2020-07-23 17:11:20 -0500

    LP3 - Sesion 12

---
+++ miapp/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render,HttpResponse, redirect
+
+# Create your views here.
+
+layout = """
+    <h1> Proyecto Web (LP3) | Flor Cerdán León </h1>
+    <hr/>
+    <ul>
+        <li>
+            <a href="/inicio"> Inicio</a>
+        </li>
+        <li>
+            <a href="/saludo"> Mensaje de Saludo</a>
+        </li>
+        <li>
+            <a href="/rango"> Mostrar Números [a,b]</a>
+        </li>
+        </ul>
+        </hr>
+"""
+def index(request):
+    return render(request, 'index.html')
+
+def saludo(request):
+    return render(request, 'saludo.html')
+def rango(request):
+    a = 10
+    b = 20
+    resultado = f"""
+        <h2> Numeros de[{a},{b}] </h2>
+        Resultado: <br>
+        <ul>
+    """
+    while a<=b:
+        resultado += f"<li>{a}</li>"
+        a+=1
+    resultado += "</ul>"
+    return HttpResponse(layout + resultado)
+def rango2(request,a=0,b=100):
+    if a>b:
+       return redirect('rango2', a=b,b=a) 
+    resultado = f"""
+        <h2> Numeros de[{a},{b}] </h2>
+        Resultado: <br>
+        <ul>
+    """
+    while a<=b:
+        resultado += f"<li>{a}</li>"
+        a+=1
+    resultado += "</ul>"
+    return HttpResponse(layout + resultado)
+
