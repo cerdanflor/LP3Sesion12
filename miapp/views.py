3a36f455c07cd58a0d4573414c29d98e0f6009e2
Daniel Levano <levanod@gmail.com>
2020-07-30 15:08:53 -0500

    Sem 13 - Estilos Básicos

--- miapp/views.py
+++ miapp/views.py
@@ -3,23 +3,7 @@ from django.shortcuts import render, HttpResponse, redirect
 # Create your views here.
 
 layout = """
-    <h1> Proyecto Web (LP3) | Flor Cerdán </h1>
-    <hr/>
-    <ul>
-        <li>
-            <a href="/inicio"> Inicio</a>
-        </li>
-        <li>
-            <a href="/saludo"> Mensaje de Saludo</a>
-        </li>
-        <li>
-            <a href="/rango"> Mostrar Números [a,b]</a>
-        </li>
-        <li>
-            <a href="/rango2/10/15"> Mostrar Números [10,15]</a>
-        </li>
-    </ul>
-    <hr/>
+
 """
 
 def index(request):
