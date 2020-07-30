260536550e41aa6fed345e1a774a4cbcec180a53
Daniel Levano <levanod@gmail.com>
2020-07-30 13:57:10 -0500

    Semana 12

--- miapp/views.py
+++ miapp/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render,HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 
 layout = """
-    <h1> Proyecto Web (LP3) | Flor Cerdán León </h1>
+    <h1> Proyecto Web (LP3) | Flor Cerdán </h1>
     <hr/>
     <ul>
         <li>
@@ -15,38 +15,46 @@ layout = """
         <li>
             <a href="/rango"> Mostrar Números [a,b]</a>
         </li>
-        </ul>
-        </hr>
+        <li>
+            <a href="/rango2/10/15"> Mostrar Números [10,15]</a>
+        </li>
+    </ul>
+    <hr/>
 """
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request,'index.html')
 
 def saludo(request):
-    return render(request, 'saludo.html')
+    return render(request,'saludo.html')
+
 def rango(request):
     a = 10
     b = 20
     resultado = f"""
-        <h2> Numeros de[{a},{b}] </h2>
+        <h2> Numeros de [{a},{b}] </h2>
         Resultado: <br>
-        <ul>
+        <ul> 
     """
+    
     while a<=b:
-        resultado += f"<li>{a}</li>"
+        resultado +=  f"<li> {a} </li>"
         a+=1
-    resultado += "</ul>"
-    return HttpResponse(layout + resultado)
-def rango2(request,a=0,b=100):
-    if a>b:
-       return redirect('rango2', a=b,b=a) 
-    resultado = f"""
-        <h2> Numeros de[{a},{b}] </h2>
-        Resultado: <br>
-        <ul>
-    """
-    while a<=b:
-        resultado += f"<li>{a}</li>"
-        a+=1
-    resultado += "</ul>"
+    resultado += "</ul"
     return HttpResponse(layout + resultado)
 
+def rango2(request,a=0,b=100):
+    if a>b:
+        return redirect('rango2',a=b, b=a)
+
+    resultado = f"""
+        <h2> Numeros de [{a},{b}] </h2>
+        Resultado: <br>
+        <ul> 
+    """
+    
+    while a<=b:
+        resultado +=  f"<li> {a} </li>"
+        a+=1
+    resultado += "</ul"
+    return HttpResponse(layout + resultado)
