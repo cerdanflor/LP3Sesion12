e57135d187f48f491b2fc282d3814633359531c9
Daniel Levano <levanod@gmail.com>
2020-08-06 15:41:22 -0500

    Semana 14

--- miapp/views.py
+++ miapp/views.py
@@ -7,25 +7,34 @@ layout = """
 """
 
 def index(request):
-    return render(request,'index.html')
+    estudiantes = [ 'Isabella Caballero',
+                    'Alejandro Hermitaño', 
+                    'Joan Palomino', 
+                    'Pierre Bernaola', 
+                    'Fabricio Condori', 
+                    'GiamPierre Huamán' ]
+    return render(request,'index.html',{
+        'titulo': 'Inicio',
+        'mensaje': 'Proyecto Web con DJango (Desde el View)',
+        'estudiantes': estudiantes
+    })
 
 def saludo(request):
-    return render(request,'saludo.html')
+    return render(request,'saludo.html',{
+        'titulo': 'Saludo',
+        'autor_saludo': 'Mg. Flor Elizabeth Cerdán León' 
+    })
 
 def rango(request):
-    a = 10
-    b = 20
-    resultado = f"""
-        <h2> Numeros de [{a},{b}] </h2>
-        Resultado: <br>
-        <ul> 
-    """
-    
-    while a<=b:
-        resultado +=  f"<li> {a} </li>"
-        a+=1
-    resultado += "</ul"
-    return HttpResponse(layout + resultado)
+   a = 10
+   b = 20
+   rango_numeros = range(a,b+1)
+   return render(request,'rango.html',{
+        'titulo': 'Rango',
+        'a': a,
+        'b': b,
+        'rango_numeros': rango_numeros 
+    })
 
 def rango2(request,a=0,b=100):
     if a>b:
